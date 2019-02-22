824f33afee3f285816b5818c70bcf58f94c2a8ef
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-22 11:23:24 +0100

    Set up server

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import 'reflect-metadata'
+import setupDb from './db'
+import { useKoaServer } from 'routing-controllers'
+import * as Koa from 'koa'
+import { Server } from 'http'
+import * as IO from 'socket.io'
+
+const app = new Koa()
+const server = new Server(app.callback())
+export const io = IO(server)
+const port = process.env.PORT || 4000
+
+
+useKoaServer(app, {
+    cors: true,
+    controllers: [
+
+    ],
+})
+
+io.on('connection', function (socket) {
+
+    console.log(`User ${socket.id} just connected`)
+
+    socket.on('movePlayer1', state => {
+        io.emit('playerOneMoved', state)
+    })
+
+    socket.on('movePlayer2', state => {
+        io.emit('playerTwoMoved', state)
+    })
+
+      socket.on('updatePuckMove', state => {
+        //   console.log(state)
+        io.emit('puckHasMoved', state)
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} just disconnected`)
+    })
+})
+
+setupDb()
+    .then(_ =>
+        server.listen(port, () => console.log(`Listening on server port ${port}`))
+    )
+    .catch(err => console.error(err)) 
